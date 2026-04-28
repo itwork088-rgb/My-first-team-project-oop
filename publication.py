e2e48ab91e17c8d20a17c62703d7499772ee9eba
Serzhan77406 <Aigul.dauletyar@mail.ru>
2026-04-28 21:23:11 +0500

    ooo

--- publication.py
+++ publication.py
@@ -1,8 +1,38 @@
+# class Publication:
+#     def __init__(self, title, author, year):
+#         self.title = title
+#         self._author = author
+#         self._year = year
+
+#     def get_info(self):
+#         return f'"{self.title}" ({self._author}, {self._year})'
+
 class Publication:
     def __init__(self, title, author, year):
-        self.title = title
-        self._author = author
-        self._year = year
+      self.is_available = True
+      self.title = title
+      self._author = author
+      self._year = year
+
+
+    def borrow(self):
+        if self.is_available:
+              self.is_available = False
+              return f'Вы взяли книгу:"{self.title}"'
+        else:
+              return "Недоступно"
+
+
 
     def get_info(self):
         return f'"{self.title}" ({self._author}, {self._year})'
+    
+# obj = Publication("wow","pushkin", 1799)    
+
+# while True:
+#     choice = int(input("1) отдолжить книгу "))
+#     if choice == 1:
+#         print(obj.borrow())
+#         print(obj.get_info())
+
+   
